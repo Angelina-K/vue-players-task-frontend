037165c3103a4cdaa094b2a0b26f8a53256a1e0c
Angelina-K <79867604+Angelina-K@users.noreply.github.com>
2022-03-01 10:08:12 +0200

    changed conn to php server

--- src/services/itemService.js
+++ src/services/itemService.js
@@ -3,10 +3,12 @@ export const itemService = {
 };
 
 const axios = require('axios');
-const DATA_URL = 'http://localhost:3000/items';
+// const DATA_URL = 'http://localhost:3000/items';
+const DATA_URL = 'http://localhost/backend/api/';
+
 
 async function query(viewBy = 'null') {
-  const res = await axios.get(`${DATA_URL}/${viewBy}`);
+  const res = await axios.get(`${DATA_URL}/${viewBy}.php`);
   const data = await res.data;
   return data;
 }
